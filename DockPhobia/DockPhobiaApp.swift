bba9dea8f3baa58977478af7aa5d39459d98b952
neon443 <69979447+neon443@users.noreply.github.com>
2025-03-13 08:31:01 +0000

    Initial Commit

---
+++ DockPhobia/DockPhobiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  DockPhobiaApp.swift
+//  DockPhobia
+//
+//  Created by neon443 on 13/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct DockPhobiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DockPhobiaTests/DockPhobiaTests.swift
@@ -0,0 +1,17 @@
+//
+//  DockPhobiaTests.swift
+//  DockPhobiaTests
+//
+//  Created by neon443 on 13/03/2025.
+//
+
+import Testing
+@testable import DockPhobia
+
+struct DockPhobiaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
