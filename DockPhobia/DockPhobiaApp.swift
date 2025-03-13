2b2aaadb8cf87b3240b73ce16555a0ca94dd2183
neon443 <69979447+neon443@users.noreply.github.com>
2025-03-13 09:05:52 +0000

    add menu bar

--- DockPhobia/DockPhobiaApp.swift
+++ DockPhobia/DockPhobiaApp.swift
@@ -9,9 +9,30 @@ import SwiftUI
 
 @main
 struct DockPhobiaApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
+	var body: some Scene {
+		MenuBarExtra(
+			"Menu Bar",
+			systemImage: "star"
+		) {
+			ContentView()
+				.overlay(alignment: .topLeading) {
+					Button() {
+						NSApp.terminate(nil)
+					} label: {
+						Image(systemName: "xmark.circle.fill")
+							.foregroundStyle(.blue)
+					}
+					.buttonStyle(PlainButtonStyle())
+				}
+				.frame(width: 300, height: 180)
+		}
+		.menuBarExtraStyle(.window)
+	}
 }
+//struct DockPhobiaApp: App {
+//    var body: some Scene {
+//        WindowGroup {
+//            ContentView()
+//        }
+//    }
+//}
