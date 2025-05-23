8d9f8294a4f2f1fceb03fd5e3598aecd0279dd58
neon443 <69979447+neon443@users.noreply.github.com>
2025-05-23 11:30:49 +0100

    printMouse() iocn

--- DockPhobia/AppDelegate.swift
+++ DockPhobia/AppDelegate.swift
@@ -9,22 +9,24 @@ import Cocoa
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
-
+	
 	@IBOutlet var window: NSWindow!
-
-
+	
+	
 	func applicationDidFinishLaunching(_ aNotification: Notification) {
 		// Insert code here to initialize your application
+		while true {
+			printMouse()
+			sleep(1)
+		}
 	}
-
+	
 	func applicationWillTerminate(_ aNotification: Notification) {
 		// Insert code here to tear down your application
 	}
-
+	
 	func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
 		return true
 	}
-
-
 }
 
---
+++ DockPhobia/MouseTracker.swift
@@ -0,0 +1,16 @@
+//
+//  MouseTracker.swift
+//  DockPhobia
+//
+//  Created by neon443 on 23/05/2025.
+//
+
+import Foundation
+import AppKit
+import Cocoa
+
+func printMouse() {
+	let mouseLoc: NSPoint
+	mouseLoc = NSEvent.mouseLocation
+	print(mouseLoc)
+}
